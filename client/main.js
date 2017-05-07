7e2a6900123dbeb16cde361d045fa017c85f0f84
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-05-07 12:42:42 +0200

    Removed typescript - added eslint-config
    We use JavaScript for this project since it is more common together with meteor.

--- client/main.js
+++ client/main.js
@@ -1,23 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var templating_1 = require("meteor/templating");
-var reactive_var_1 = require("meteor/reactive-var");
-require("./main.html");
-function getTemplateProperty(tmpl, property) {
-    return tmpl[property];
-}
-templating_1.Template.hello.onCreated(function helloOnCreated() {
-    this.counter = new reactive_var_1.ReactiveVar(0);
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
 });
-templating_1.Template.hello.helpers({
-    counter: function () {
-        return getTemplateProperty(templating_1.Template.instance(), 'counter').get();
-    },
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
 });
-templating_1.Template.hello.events({
-    'click button': function (event, instance) {
-        var counter = getTemplateProperty(instance, 'counter');
-        counter.set(counter.get() + 1);
-    },
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    let counter = instance.counter;
+    counter.set(counter.get() + 1);
+  },
 });
-//# sourceMappingURL=main.js.map
