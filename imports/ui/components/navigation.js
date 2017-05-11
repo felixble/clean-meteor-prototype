05f585faf719dbec8b39b293a8f4171024a51e28
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-05-11 22:57:54 +0200

    Some navigation improvements

--- imports/ui/components/navigation.js
+++ imports/ui/components/navigation.js
@@ -1,1 +1,11 @@
 import './navigation.html';
+
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+Template.navigation.helpers({
+    isRouteActive(route) {
+        const currentRoute = FlowRouter.getRouteName();
+        return (route === currentRoute) ? 'active' : '';
+    }
+});
