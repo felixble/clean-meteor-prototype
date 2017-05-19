64e5a3ae9f00ac6115eed25c190ed89c022bc34a
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-05-19 13:41:38 +0200

    Show dashboard reactive if user with same session (different tab, same browser) logs in

--- imports/ui/pages/login.js
+++ imports/ui/pages/login.js
@@ -1,1 +1,14 @@
 import './login.html';
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { redirectAfterLogin } from '../../startup/client/routes';
+
+
+Template.login.onCreated(function() {
+    this.autorun(function() {
+        if (Meteor.userId()) {
+            FlowRouter.go(redirectAfterLogin);
+        }
+    });
+});
