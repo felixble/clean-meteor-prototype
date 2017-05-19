1287a50db2e8cb7580777e4f86c00954be2c427f
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-05-19 13:39:27 +0200

    Reverted using use-case in publication
    This leads to reactivity issues. We have to think about a different concept...

--- imports/api/articles/server/publications.js
+++ imports/api/articles/server/publications.js
@@ -1,16 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import {meteorBaseFactory} from '../../../infrastructure/meteor-base-factory';
-import {EditListUseCase} from '../../../core/use-cases/edit-list-use-case';
 
 
 Meteor.publish('articles', function() {
-    //return meteorBaseFactory.getArticleRepository().fetchAllArticles();
-    const usecase = new EditListUseCase(meteorBaseFactory.getInstanceWithContext(this));
-    let asyncFnWithCallback = (callback) => {
-        usecase.listAllArticles()
-            .then(res => callback(null, res))
-            .catch(err => callback(err));
-    };
-    let res = Meteor.wrapAsync(asyncFnWithCallback);
-    return res();
+    return meteorBaseFactory.getArticleRepository().fetchAllArticles();
 });
